01002e5dc3451f13f0a11ef812d428d0841353e6
Yuki Fujita <uni22yu@gmail.com>
2022-12-21 16:47:36 +0900

    home appBar

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # my_twitter_ui
 これはTwitterのUIを模したFlutterアプリです。
+
+# 以下、開発時のメモ・参考サイト
+## エラー対応・機能の実装方法時めも
+### CircleAvatarでassets imageを使用する
+```
+  CircleAvatar(
+      child: Image.asset('assets/horse.png'),
+   );
+```
+
+or
+
+```
+CircleAvatar(
+  backgroundImage: AssetImage('assets/horse.png'),
+);
+```
+[StackOverflow](https://stackoverflow.com/questions/54298886/how-to-give-a-circleavatar-an-image-from-assets)
+
+## 便利サイト
+[Iconの一覧](https://api.flutter.dev/flutter/material/Icons-class.html)
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -12,7 +13,17 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Twitter Home'),
+        leading: const CircleAvatar(
+          radius: 50,
+          backgroundColor: Colors.white,
+          backgroundImage: AssetImage('images/my_icon_image.png'),
+        ),
+        title: FaIcon(FontAwesomeIcons.twitter),
+        actions: <Widget>[
+          IconButton(
+						onPressed:() => print("The button has pressed"),
+						icon: Icon(Icons.auto_awesome))
+        ],
       ),
       body: Center(
         child: Image.asset('images/my_icon_image.png'),
