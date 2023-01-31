885185ac016d43353d7a3d2a309b52ffa58fe946
Yuki Fujita <uni22yu@gmail.com>
2023-01-31 23:02:08 +0900

    drawer and bottomnavigationbar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:my_twitter_ui/model/tweet.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,35 +10,41 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var _scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
-        leading: const CircleAvatar(
-          radius: 30,
-          backgroundColor: Colors.blue,
-          backgroundImage: AssetImage('images/my_icon_image.png'),
+        leading: TextButton(
+          onPressed: () {
+            _scaffoldKey.currentState!.openDrawer();
+          },
+          child: Image.asset('images/my_icon_image.png'),
         ),
-        // ignore: prefer_const_constructors
-        title: FaIcon(
-					FontAwesomeIcons.twitter,
-					color: Colors.blue
-				),
+        title: FaIcon(FontAwesomeIcons.twitter, color: Colors.blue),
         actions: <Widget>[
           IconButton(
-						onPressed:() => print("The button has pressed"),
-      // ignore: prefer_const_constructors
-						icon: Icon(
-							Icons.auto_awesome,
-							color: Colors.black
-							))
+              onPressed: () => print("The button has pressed"),
+              icon: Icon(Icons.auto_awesome_outlined, color: Colors.black))
         ],
-				backgroundColor: Colors.white,
+        backgroundColor: Colors.white,
       ),
       body: Center(
-        child: Image.asset('images/my_icon_image.png'),
+        child: Column(
+          children: [Text('first'), Text('second')],
+        ),
       ),
-
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.notifications_outlined), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.mail_outline), label: ""),
+        ],
+        type: BottomNavigationBarType.fixed,
+      ),
+      drawer: Drawer(child: Center(child: Text('drawer'))),
     );
   }
 }
