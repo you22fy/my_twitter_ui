e0b2bfb897a4974c2cc5fe71143e04ff585c4bbb
Yuki Fujita <uni22yu@gmail.com>
2023-01-31 23:49:16 +0900

    add User class

--- lib/model/tweet.dart
+++ lib/model/tweet.dart
@@ -2,9 +2,12 @@ class Tweet {
   String tweet;
   DateTime sendTime;
 
-  Tweet({
-    required this.tweet,
-    required this.sendTime
-  });
+  Tweet({required this.tweet, required this.sendTime});
 }
 
+class User {
+  String name;
+  String id;
+
+  User({required this.name, required this.id});
+}
